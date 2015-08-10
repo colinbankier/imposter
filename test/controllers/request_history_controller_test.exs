711c34315105bc35ed061cd14774c9c49f6989e7
Mbuckley0 <m5.buckley@connect.qut.edu.au>
2015-08-10 15:30:19 +1000

    Add and fix up tests

--- test/controllers/request_history_controller_test.exs
+++ test/controllers/request_history_controller_test.exs
@@ -1,36 +1,49 @@
 defmodule Imposter.RequestHistoryControllerTest do
-	use Imposter.ConnCase
+  use Imposter.ConnCase
 
-	alias Imposter.RequestHistory
+  @attrs %{method: "GET", path: "/baz", response: %{foo: "this is a get"}}
 
-	@attrs %{method: "GET", path: "/baz", response: %{foo: "this is a get"}}
+  setup do
+    on_exit fn ->
+      delete conn, request_history_path(conn, :delete_all)
+      delete conn, routes_path(conn, :delete_all)
+    end
 
-	setup do
-		conn = conn() |> put_req_header("accept", "application/json")
-		{:ok, conn: conn}
-	end
-
-	test "lists all entries on index", %{conn: conn} do
-		conn = get conn, request_history_path(conn, :index)
-    assert conn.status == 200
-    assert conn.resp_body == "[]"
-	end
-
-	test "shows chosen resource", %{conn: conn} do
-	  conn = post conn, routes_path(conn, :create), @attrs
-	  conn = get conn, "/baz"
-	  conn = get conn, request_history_path(conn, :show, 0)
-  	assert conn.status == 200
-  	assert conn.resp_body == "{\"path\":\"/baz\",\"method\":\"GET\",\"body\":{}}"
-  	conn = delete conn, request_history_path(conn, :delete_all)
-  	conn = delete conn, routes_path(conn, :delete_all)
+    conn = conn() |> put_req_header("accept", "application/json")
+    {:ok, conn: conn}
   end
 
-	test "deletes all chosen resources", %{conn: conn} do
-		conn = post conn, routes_path(conn, :create), @attrs
-	  conn = get conn, "/baz"
-		conn = delete conn, request_history_path(conn, :delete_all)
-  	assert conn.status == 200
-		conn = delete conn, routes_path(conn, :delete_all)
+  test "lists all entries on index", %{conn: conn} do
+    conn = get conn, request_history_path(conn, :index)
+    assert conn.status == 200
+    assert conn.resp_body == "[]"
+  end
+
+  test "lists an actual entry on index", %{conn: conn} do
+    conn = post conn, routes_path(conn, :create), @attrs
+    conn = get conn, "/baz"
+
+    conn = get conn, request_history_path(conn, :index)
+    assert conn.status == 200
+    assert conn.resp_body == "[{\"path\":\"/baz\",\"method\":\"GET\",\"body\":{}}]"
+  end
+
+  test "shows chosen resource", %{conn: conn} do
+    conn = post conn, routes_path(conn, :create), @attrs
+    conn = get conn, "/baz"
+
+    conn = get conn, request_history_path(conn, :show, 0)
+    assert conn.status == 200
+    assert conn.resp_body == "{\"path\":\"/baz\",\"method\":\"GET\",\"body\":{}}"
+  end
+
+  test "deletes all chosen resources", %{conn: conn} do
+    conn = post conn, routes_path(conn, :create), @attrs
+    conn = get conn, "/baz"
+
+    conn = delete conn, request_history_path(conn, :delete_all)
+    conn = get conn, request_history_path(conn, :index)
+    assert conn.status == 200
+    assert conn.resp_body == "[]"
   end
 end
--- test/controllers/routes_controller_test.exs
+++ test/controllers/routes_controller_test.exs
@@ -1,14 +1,17 @@
 defmodule Imposter.RoutesControllerTest do
-	use Imposter.ConnCase
+  use Imposter.ConnCase
 
-  alias Imposter.Routes
+  @attrs %{method: "GET", path: "/baz", response: %{ body: %{foo: "this is a get"}}}
 
-	@attrs %{method: "GET", path: "/baz", response: %{ body: %{foo: "this is a get"}}}
+  setup do
+    on_exit fn ->
+      delete conn, request_history_path(conn, :delete_all)
+      delete conn, routes_path(conn, :delete_all)
+    end
 
-	setup do
-		conn = conn() |> put_req_header("accept", "application/json")
-		{:ok, conn: conn}
-	end
+    conn = conn() |> put_req_header("accept", "application/json")
+    {:ok, conn: conn}
+  end
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, routes_path(conn, :index)
@@ -16,15 +19,23 @@ defmodule Imposter.RoutesControllerTest do
     assert conn.resp_body == "[]"
   end
 
+  test "lists an actual entry on index", %{conn: conn} do
+    conn = post conn, routes_path(conn, :create), @attrs
+
+    conn = get conn, routes_path(conn, :index)
+    assert conn.status == 200
+    assert conn.resp_body == "[{\"response\":{\"status_code\":200,\"headers\":[],\"body\":{\"foo\":\"this is a get\"}},\"path\":\"/baz\",\"method\":\"GET\"}]"
+  end
+
   test "creates and renders resource when data is valid", %{conn: conn} do
-  	conn = post conn, routes_path(conn, :create), @attrs
-  	assert conn.status == 200
+    conn = post conn, routes_path(conn, :create), @attrs
+    assert conn.status == 200
     assert conn.resp_body == "{\"path\":\"/baz\",\"method\":\"GET\"}"
-  	conn = delete conn, routes_path(conn, :delete_all)
   end
 
   test "deletes all chosen resources", %{conn: conn} do
     conn = post conn, routes_path(conn, :create), @attrs
+
     conn = delete conn, routes_path(conn, :delete_all)
     conn = get conn, routes_path(conn, :index)
     assert conn.status == 200
